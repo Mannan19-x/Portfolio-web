1acace74ff10911b1d3e14f3e35aa0c3a454dfff
Mannan19-x <mannanjain212007@gmail.com>
2026-04-29 11:38:56 +0530

    Add files via upload

---
+++ Portfolio web/script.js
@@ -0,0 +1,20 @@
+function scrollToWork() {
+  document.getElementById("work").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+/* Scroll animation */
+window.addEventListener("scroll", () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    const position = card.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100) {
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
